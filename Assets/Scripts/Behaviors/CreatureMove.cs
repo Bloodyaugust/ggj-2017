b4fd4fd50c25d226ae2e44f450275a291371dd8c
bloodyaugust <greysonrichey@gmail.com>
2017-01-22 12:48:24 -0700

    Modified creature movement to auto-select a new castle target.

--- Assets/Scripts/Behaviors/CreatureMove.cs
+++ Assets/Scripts/Behaviors/CreatureMove.cs
@@ -6,23 +6,47 @@ public class CreatureMove : MonoBehaviour {
 
 	public float MoveSpeed;
 
+	PlayerConfig _playerConfig;
+	GameObject[] _castles;
 	GameObject _target;
 	CreatureState _state;
 
 	void Awake () {
 		_state = GetComponent<CreatureState>();
+		_playerConfig = GetComponent<PlayerConfig>();
 	}
 
 	// Use this for initialization
 	void Start () {
-
+		_castles = GameObject.FindGameObjectsWithTag("Castle");
 	}
 
 	// Update is called once per frame
 	void Update () {
 		Vector3 newScale;
+		float currentDistance;
+		float testingDistance;
 
 		if (_state.GetState() == (int)CreatureState.States.Moving) {
+			if (_target == null) {
+				currentDistance = 99999999999;
+				testingDistance = 0;
+				_castles = GameObject.FindGameObjectsWithTag("Castle");
+
+				for (int i = 0; i < _castles.Length; i++) {
+					if (_castles[i].GetComponent<PlayerConfig>().PlayerID != _playerConfig.PlayerID) {
+						testingDistance = Vector3.Distance(_castles[i].transform.position, transform.position);
+
+						if (testingDistance < currentDistance) {
+							_target = _castles[i];
+							currentDistance = testingDistance;
+						}
+					}
+				}
+
+				SetTarget(_target);
+			}
+
 			transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, MoveSpeed * Time.deltaTime);
 
 			if (transform.position.x < _target.transform.position.x) {
