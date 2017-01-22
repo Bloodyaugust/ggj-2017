3eb1344b95cf47e2184aa260dca86395ea1d9dd9
bloodyaugust <greysonrichey@gmail.com>
2017-01-22 13:49:58 -0700

    Added but not implemented spritecarousel for damaged castles. Corrected castle positions. Bumped range of melee units so the can attack castles.

---
+++ Assets/Scripts/Behaviors/SpriteCarousel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteCarousel : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public int currentIndex;
+
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		currentIndex = (int)Mathf.Floor(Time.time % sprites.Length);
+		spriteRenderer.sprite = sprites[currentIndex];
+	}
+}
