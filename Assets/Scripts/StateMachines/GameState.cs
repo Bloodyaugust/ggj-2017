56ba70d9fd5696b7475f4472a521286dbdec1ecf
greyson <greyson@l4digital.com>
2017-01-29 00:58:57 -0700

    New GameState to manage rounds.

---
+++ Assets/Scripts/StateMachines/GameState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+	public enum States {
+		SpawnQueue,
+		Combat
+	};
+
+	int _lastState = 0;
+	public int _currentState = 0;
+	bool _stateChanged = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void LateUpdate () {
+		if (_stateChanged) {
+			_stateChanged = false;
+		}
+	}
+
+	public int GetState () {
+		return _currentState;
+	}
+
+	public void SetState (int newState) {
+		if (_currentState != newState) {
+			_lastState = _currentState;
+			_currentState = newState;
+
+			_stateChanged = true;
+		}
+	}
+}
